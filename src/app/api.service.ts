2a5aed72c733d278757649105c6186b0c5abe62f
Nevin Joseph <nevinthoppil@hotmail.com>
2025-01-09 16:03:43 +0100

    Users can select their location

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -94,10 +94,20 @@ export class ApiService {
     });
   }
 
-  // Update location
-  updateLocation(userId: string, location: string): Observable<any> {
-    return this.http.put<any>(`${this.baseUrl}/users/${userId}/location`, { location });
+  // Update User Location
+  updateLocation(userId: string, locationData: [number, number]): Observable<any> {
+    console.log('Sending location data to API:', locationData); // Log the data being sent
+    const [latitude, longitude] = locationData;
+    const formattedData = { latitude, longitude }; // Convert array to object
+
+    let headers = this.getAuthHeaders();
+    console.log('Headers being sent:', headers);
+
+    return this.http.put<any>(`${this.baseUrl}/users/${userId}/location`, formattedData, {
+        headers,
+    });
   }
+
 
   getUsernames(): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/users`);
